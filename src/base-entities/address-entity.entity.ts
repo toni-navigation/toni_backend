9506e2f03b5e5ef97f20806fc57d14c90fd73be2
Julian Fleig <fleig.julian@googlemail.com>
2024-10-04 14:02:32 +0200

    Add GeoJson

--- src/base-entities/address-entity.entity.ts
+++ src/base-entities/address-entity.entity.ts
@@ -1,5 +1,6 @@
-import { Column } from 'typeorm';
+import { Column, Index } from 'typeorm';
 import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
+import { PointDto } from '@/authentication/dto/point.dto';
 
 export class Address {
   @IsString()
@@ -19,6 +20,14 @@ export class Address {
   city: string;
 
   // Use a string to store the geography data in WKT (Well-Known Text) format
-  @Column({ type: 'geography', spatialFeatureType: 'Point', srid: 4326 }) // SRID 4326 for WGS84
-  geolocation: number[]; // or use number[] for coordinates
+  // @Column({ type: 'geography', spatialFeatureType: 'Point', srid: 4326 }) // SRID 4326 for WGS84
+  // geolocation: number[]; // or use number[] for coordinates
+
+  @Index({ spatial: true })
+  @Column('geometry', {
+    spatialFeatureType: 'Point',
+    srid: 4326,
+  })
+  geometry: PointDto;
+
 }
