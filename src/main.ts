1696d2f1665fef8694a325eb9b552a516b07e946
Julian Fleig <fleig.julian@googlemail.com>
2024-10-29 17:41:04 +0100

    Working Bearer

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser());
-  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Toni API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
 
-  const config = new DocumentBuilder().setTitle('Toni API').addBearerAuth().build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
 }
-
 bootstrap();
