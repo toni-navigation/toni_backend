3e8503d9465260b11da19c4799d9351e0bd5bf97
Lisa Rader <lis.rader99@gmail.com>
2025-02-03 11:24:31 +0100

    Change entities in postgresConntection

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -14,7 +14,7 @@ export const postgresConnectionOptions: PostgresConnectionOptions = {
   url:
     configService.get('DATABASE_URL', { infer: true }) ||
     `postgres://${configService.get('DB_USERNAME', { infer: true })}:${configService.get('DB_PASSWORD', { infer: true })}@${configService.get('DB_HOST', { infer: true })}:${configService.get('DB_PORT', { infer: true })}/${configService.get('DB_DATABASE', { infer: true })}`,
-  entities: [configService.get('TYPEORM_ENTITIES', { infer: true })],
+  entities: process.env.TYPEORM_ENTITIES?.split(','),
   migrations: [configService.get('TYPEORM_MIGRATIONS', { infer: true })],
   migrationsRun: configService.get('TYPEORM_MIGRATIONS_RUN', { infer: true }),
   synchronize: configService.get('TYPEORM_SYNCHRONIZE', { infer: true }),
