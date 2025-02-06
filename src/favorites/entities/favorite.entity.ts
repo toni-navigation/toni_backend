c0046fa9b5a82211e35909349e11d42a3f99d65e
Julian Fleig <fleig.julian@googlemail.com>
2025-02-06 10:47:07 +0100

    No unique Favorite Title

--- src/favorites/entities/favorite.entity.ts
+++ src/favorites/entities/favorite.entity.ts
@@ -9,7 +9,7 @@ import { User } from '@/users/entities/user.entity';
 
 @Entity()
 export class Favorite extends BaseEntity {
-  @Column('text', { unique: true })
+  @Column('text')
   title: string;
 
   @Column({
